d82074147653e78517c93c460558a0523eafe00a
SouchenOu <soukainaouchenuai@gmail.com>
2024-04-18 13:29:25 -0400

    finish Travel website

--- new-app/components/footer.tsx
+++ new-app/components/footer.tsx
@@ -1,20 +1,85 @@
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
+
+type PropsVar = {
+  title: string;
+  children: React.ReactNode;
+}
+
+const FooterColumn = ({ title, children }: PropsVar) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  )
+}
+
 const Footer = () => {
   return (
-    <footer className='border-2 border-red-700 flex items-center justify-center mb-24'>
-        <div className='w-full px-6 lg:px-20 3xl:px-0 flex flex-col gap-14 mx-auto max-w-[1440px] '>
-          <div className='flex flex-col items-start'>
-            <Link href="/" className=''>
-              <Image src="hilink-logo.svg" alt="" width={74} height={29}/>
-            </Link>
+    <footer className="flex items-center justify-center my-[80px]">
+      <div className="px-6 lg:px-20 3xl:px-0 mx-auto max-w-[1440px] flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="/1test.png" alt="logo" width={74} height={29}/>
+          </Link>
 
+          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link) => (
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link) => (
+                  <Link
+                    href="/"
+                    key={link.label}
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
+                    <p className="whitespace-nowrap">
+                      {link.label}:
+                    </p>
+                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                      {link.value}
+                    </p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={SOCIALS.title}>
+                <ul className="regular-14 flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link) => (
+                    <Link href="/" key={link}>
+                      <Image src={link} alt="logo" width={24} height={24} />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
           </div>
         </div>
+
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">2024 | Made by souchen</p>
+      </div>
     </footer>
   )
 }
+
+
 
 export default Footer
